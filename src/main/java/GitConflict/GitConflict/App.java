f067494b3cddedb52d7b1e68daab1b26e02105a0
venkataprasad <prasadbabu457@gmail.com>
2020-07-22 12:10:40 +0530

    commit the message

---
+++ src/main/java/GitConflict/GitConflict/App.java
@@ -0,0 +1,13 @@
+package GitConflict.GitConflict;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
